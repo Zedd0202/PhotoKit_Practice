0d739e9091881f1afce4a2c859b9aec0ea0fcc19
Zedd <zedd.0202@kakaocorp.com>
2018-11-09 17:58:32 +0900

    PhotoKit

---
+++ PhotoKitPractice/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  PhotoKitPractice
+//
+//  Created by Zedd on 27/10/2018.
+//  Copyright © 2018 Zedd. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    var allPhotos: PHFetchResult<PHAsset>?
+    let scale = UIScreen.main.scale
+    var thumbnailSize = CGSize.zero
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        self.allPhotos = PHAsset.fetchAssets(with: nil)
+        self.collectionView.reloadData()
+        self.thumbnailSize = CGSize(width: 1024 * scale, height: 1024 * scale)
+    }
+
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.allPhotos?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AssetsCollectionViewCell", for: indexPath) as! AssetsCollectionViewCell
+        
+       //let asset = self.allPhotos?.object(at: indexPath.item)
+        let asset = self.allPhotos?[indexPath.item]
+        LocalImageManager.shared.requestIamge(with: asset, thumbnailSize: self.thumbnailSize) { (image) in
+           cell.configure(with: image)
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGSize(width: self.view.frame.width / 3, height: 100)
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+}
+
+class AssetsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var assetImageView: UIImageView!
+    fileprivate let imageManager = PHImageManager()
+
+    var representedAssetIdentifier: String?
+    
+    var thumbnailSize: CGSize {
+        let scale = UIScreen.main.scale
+        return CGSize(width: (UIScreen.main.bounds.width / 3) * scale, height: 100 * scale)
+        
+    }
+    
+    func configure(with image: UIImage?) {
+        self.assetImageView.image = image
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
+final class LocalImageManager {
+    
+    static var shared = LocalImageManager()
+    
+    fileprivate let imageManager = PHImageManager()
+    
+    var representedAssetIdentifier: String?
+    
+    func requestIamge(with asset: PHAsset?, thumbnailSize: CGSize, completion: @escaping (UIImage?) -> Void) {
+        guard let asset = asset else {
+            completion(nil)
+            return
+        }
+        self.representedAssetIdentifier = asset.localIdentifier
+        self.imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, info in
+            // UIKit may have recycled this cell by the handler's activation time.
+          //  print(info?["PHImageResultIsDegradedKey"])
+            // Set the cell's thumbnail image only if it's still showing the same asset.
+            if self.representedAssetIdentifier == asset.localIdentifier {
+               completion(image)
+            }
+        })
+    }
+}
